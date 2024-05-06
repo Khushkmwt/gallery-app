a1430a9ef3b9c878f50e99d674c8e674017445d6
Dilkhush Kumawat <dilkhushkumawat33@gmail.com>
2024-05-07 01:38:47 +0530

    proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/api":"https://gallery-app-server2.vercel.app/"
+      "/api":"https://gallery-app-server2.vercel.app"
     }
   },
   plugins: [react()],
