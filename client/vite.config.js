142bf61c7aec360a542e2c408005513dac93cb50
Dilkhush Kumawat <dilkhushkumawat33@gmail.com>
2024-05-07 12:08:10 +0530

    proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       "/api": {
         target: "https://gallery-app-server2.vercel.app",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        rewrite: (path) => '/api' + path,
       }
     }
   },
